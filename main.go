dc6f061871b0e60942c6f180daf94754ede58067
r3g492 <kangggunw3492@gmail.com>
2024-03-02 19:40:21 +0900

    implement draw and move

--- main.go
+++ main.go
@@ -1,19 +1,61 @@
 package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	"fmt"
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
 
 func main() {
 	rl.InitWindow(800, 450, "raylib [core] example - basic window")
 	defer rl.CloseWindow()
-
 	rl.SetTargetFPS(60)
 
+	var time float32 = 0.0
+	var objectsList []interface{}
+	objectsList = append(objectsList, rl.NewRectangle(400, 200, 100, 100))
+
 	for !rl.WindowShouldClose() {
-		rl.BeginDrawing()
+		move(&objectsList)
+		draw(&time, objectsList)
+	}
+}
 
-		rl.ClearBackground(rl.Black)
-		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.Blue)
+func draw(time *float32, objectsList []interface{}) {
+	rl.BeginDrawing()
 
-		rl.EndDrawing()
+	rl.ClearBackground(rl.Black)
+	timeText := fmt.Sprintf("%02.02f seconds", *time)
+	rl.DrawText("cur time: "+timeText, 650, 1, 5, rl.Green)
+
+	for _, obj := range objectsList {
+		rl.DrawRectangleRec(obj.(rl.Rectangle), rl.Red)
+	}
+
+	rl.EndDrawing()
+
+	*time += rl.GetFrameTime()
+}
+
+func move(objectsList *[]interface{}) {
+	// get input
+	if rl.IsKeyDown(rl.KeyRight) {
+		for i, obj := range *objectsList {
+			(*objectsList)[i] = rl.NewRectangle(obj.(rl.Rectangle).X+1, obj.(rl.Rectangle).Y, obj.(rl.Rectangle).Width, obj.(rl.Rectangle).Height)
+		}
+	}
+	if rl.IsKeyDown(rl.KeyLeft) {
+		for i, obj := range *objectsList {
+			(*objectsList)[i] = rl.NewRectangle(obj.(rl.Rectangle).X-1, obj.(rl.Rectangle).Y, obj.(rl.Rectangle).Width, obj.(rl.Rectangle).Height)
+		}
+	}
+	if rl.IsKeyDown(rl.KeyUp) {
+		for i, obj := range *objectsList {
+			(*objectsList)[i] = rl.NewRectangle(obj.(rl.Rectangle).X, obj.(rl.Rectangle).Y-1, obj.(rl.Rectangle).Width, obj.(rl.Rectangle).Height)
+		}
+	}
+	if rl.IsKeyDown(rl.KeyDown) {
+		for i, obj := range *objectsList {
+			(*objectsList)[i] = rl.NewRectangle(obj.(rl.Rectangle).X, obj.(rl.Rectangle).Y+1, obj.(rl.Rectangle).Width, obj.(rl.Rectangle).Height)
+		}
 	}
 }
---
+++ model/rectangle.go
@@ -0,0 +1,8 @@
+package model
+
+type rectangle struct {
+	x      int32
+	y      int32
+	width  int32
+	height int32
+}
